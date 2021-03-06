da1949129f974876cf2ac454d719e6972a507c79
thanos <koutroubas.thanos@gmail.com>
2021-03-06 02:33:12 +0200

    Added welcome and info message on kernel startup

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -18,7 +18,32 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
 	(void) atags;
 
 	uart_init();
-	uart_puts("Hello, beautiful kernel World!\r\n");
+
+	/* Print Initial Starting Message to Serial Console */
+	uart_puts("                  _____ _____ \r\n");
+	uart_puts(" ___ ___ _____   |     |   __|\r\n");
+	uart_puts("| . |  _|     |  |  |  |__   |\r\n");
+	uart_puts("|__,|_| |_|_|_|  |_____|_____|\r\n");
+
+	uart_puts("\r\nCustom ARM OS initializing...\r\n");
+
+#ifdef MODEL_0
+	uart_puts("\r\n\tBoard: Raspberry Pi Zero / Zero W\r\n");
+#elif MODEL_2
+	uart_puts("\r\n\tBoard: Raspberry Pi 2\r\n");
+#elif MODEL_3
+	uart_puts("\r\n\tBoard: Raspberry Pi 3\r\n");
+#elif MODEL_4
+	uart_puts("\r\n\tBoard: Raspberry Pi 4\r\n");
+#endif
+
+#ifdef AARCH_32
+	uart_puts("\tArch: aarch32\r\n");
+#elif AARCH_64
+	uart_puts("\tArch: aarch64\r\n");
+#endif
+	uart_puts("Done\r\n");
+
 
 	while (1) {
 		uart_putc(uart_getc());
